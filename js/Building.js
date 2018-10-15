fefe2e71a3fc8b910db9d81cc8f33837b95e0c7c
Lizhou Cai <cailizhou1006@hotmail.com>
2018-10-15 17:55:27 -0400

    Keep track of time

--- js/Building.js
+++ js/Building.js
@@ -1,7 +1,14 @@
 let Building = function(X,Y,type,lv=0){
+    this.yield=0;
     if(type=="hotel"){
         X-=150; 
         Y-=140;
+        this.yield=50*Math.pow(2,lv-1);
+    }else if(type=="oil"){
+        this.yield=50*Math.pow(2,lv-1);
+    }else if(type=="house"){
+        this.yield=5+lv*5;
+
     }
     this.x=X;
     this.y=Y;
@@ -13,7 +20,34 @@ let Building = function(X,Y,type,lv=0){
     this.sprite=game.add.sprite(X, Y, name);
     this.sprite.inputEnabled = true;
     this.lv=lv;
-    // this.sprite.scale.setTo();
+    
+    this.popText = game.add.text(0, 0, '0', {
+        font: '48px Arial Black',
+        fill: '#fff',
+        strokeThickness: 4
+    });
+    this.popText.exists = false;
+    this.popText.tween = game.add.tween(this.popText)
+        .to({
+            alpha: 0,
+            y: this.sprite.centerY-250,
+            x: this.sprite.centerX+100
+        }, 1200, Phaser.Easing.Cubic.Out);
+
+        this.popText.tween.onComplete.add(function(text, tween) {
+        text.kill();
+    });
+};
+
+Building.prototype.yieldMoney=function(){
+    if(this.yield==0)return;
+    if (this.popText) {
+        this.popText.text = this.yield;
+        this.popText.reset(this.sprite.centerX+100, this.sprite.centerY-150);
+        this.popText.alpha = 1;
+        this.popText.tween.start();
+    }
+    money+=this.yield;
 };
 Building.prototype.upgradable=function(){
     return this.lv>0 && this.lv<3;
--- js/HUD.js
+++ js/HUD.js
@@ -1,14 +1,16 @@
 
 let HUD=function() {
-
+    this.moneyInfo = game.add.text(2000, 15, 'Money: 0', { font: "48px Arial", fill: '#ffffff' });
+    this.PopuInfo = game.add.text(2000, 65, 'Population: 0', { font: "48px Arial", fill: '#ffffff' });
+    this.timeInfo = game.add.text(1100, 15, '0', { font: "96px Arial", fill: '#ffffff' });
+    this.moneyInfo.fixedToCamera = true;
+    this.PopuInfo.fixedToCamera = true;
+    this.timeInfo.fixedToCamera = true;
 };
-
-HUD.prototype.updateHud=function(){
-    console.log("in updatehud");
-
-};
-
-HUD.prototype.doss=function(){
-    console.log("Hello world!");
-    return "butt";
+HUD.prototype.updateTime=function(time){
+    this.timeInfo.text=String(Math.floor(time/60))+":"+String(time%60).padStart(2, '0');
+}
+HUD.prototype.updateHud=function(money,population){
+    this.moneyInfo.text='Money: '+String(money);
+    this.PopuInfo.text='Population: '+String(population);
 };
