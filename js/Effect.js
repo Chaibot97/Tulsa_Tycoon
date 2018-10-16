0605d5674ad72df96a987f5de8074e2fea01886c
Anthony Veschi <anthonyveschi@gmail.com>
2018-10-16 12:38:33 -0400

    continue not worrying about it

--- js/Effect.js
+++ js/Effect.js
@@ -2,7 +2,7 @@ let Effect = function(State){
 
 	this.text = game.add.text(1000, 500, State);
 	this.text.fixedToCamera = true;
-	this.text.cameraOffset.setTo(1000, 200);
+	this.text.cameraOffset.setTo(1000, 350);
 	this.text.font = 'Baskerville';	//Bastion?
 	this.text.fill = '#65390D';
 	this.text.fontSize = 64;
