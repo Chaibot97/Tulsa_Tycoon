5d95e34983e801b2f6b9e3e87e019ab81102fb63
Anthony Veschi <anthonyveschi@gmail.com>
2018-10-16 12:36:23 -0400

    changed like one singular number don't worry about it

--- js/Effect.js
+++ js/Effect.js
@@ -1,6 +1,6 @@
 let Effect = function(State){
 
-	this.text = game.add.text(1000, 400, State);
+	this.text = game.add.text(1000, 500, State);
 	this.text.fixedToCamera = true;
 	this.text.cameraOffset.setTo(1000, 200);
 	this.text.font = 'Baskerville';	//Bastion?
